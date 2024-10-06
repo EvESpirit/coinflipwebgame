76f734b11f5b63bdd21ea9d55ea171e81f01ac8f
EvESpirit <40306186+EvESpirit@users.noreply.github.com>
2024-10-06 18:02:11 +0200

    Update app.py
    2024 update to ensure it still even fkin works

--- app.py
+++ app.py
@@ -1,46 +1,31 @@
 import os
-from flask import flash, redirect, url_for, render_template, request, Flask, session, jsonify
-from flask_login import login_user, logout_user, login_required, current_user, LoginManager
-from werkzeug.security import generate_password_hash, check_password_hash
-from models import db, User  # Update this line
-import secrets
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, LoginManager
-from waitress import serve
 import json
+import secrets
+from flask import Flask, flash, redirect, url_for, render_template, request, session, jsonify
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import db, User
+from waitress import serve
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', '8weq789weq78qwe4fg4fg564jfj854ukl')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///coin_flip_game.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-
-app.secret_key = '8weq789weq78qwe4fg4fg564jfj854ukl'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///coin_flip_game.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-secretsGenerator = secrets.SystemRandom()
-
-with app.app_context():
-    db.create_all()
-
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+secretsGenerator = secrets.SystemRandom()
+
+with app.app_context():
+    db.create_all()
 
 def get_coin_toss_results_and_balance_graph(user_id):
     user = User.query.get(user_id)
@@ -51,7 +36,6 @@ def get_coin_toss_results_and_balance_graph(user_id):
     balance_graph_data = [{'x': i, 'y': result['balance']} for i, result in enumerate(user.coin_toss_results)]
 
     return coin_toss_results, balance_graph_data
-
 
 def read_balance(file_name):
     if os.path.exists(file_name):
@@ -64,11 +48,9 @@ def read_balance(file_name):
         balance = 1.0
     return round(balance, 2)
 
-
 def write_balance(file_name, balance):
     with open(file_name, 'w') as file:
         file.write(str(round(balance, 2)))
-
 
 def update_user_coin_toss_results(user, coin_result, bet_amount, balance):
     user_coin_toss_results = user.coin_toss_results
@@ -76,7 +58,6 @@ def update_user_coin_toss_results(user, coin_result, bet_amount, balance):
         {'coin_result': coin_result, 'bet_amount': bet_amount, 'balance': balance})
     user.coin_toss_results = user_coin_toss_results[-20:]  # Store only the last 20 results
     db.session.commit()
-
 
 def coin_flip(balance, bet_amount):
     flip = secretsGenerator.randint(0, 1)
@@ -95,7 +76,6 @@ def coin_flip(balance, bet_amount):
     else:
         session['coin_result'] = coin_result
         return round(new_balance, 4)
-
 
 @app.route('/', methods=['GET', 'POST'])
 @login_required
@@ -156,13 +136,11 @@ def play_game():
     return render_template('index.html', balance=balance, coin_result=session['coin_result'],
                            balance_history=session['balance_history'], bet_amount=session.get('bet_amount', 0))
 
-
 @app.route('/render_game')
 def render_game():
     balance = session['balance']
     return render_template('index.html', balance=balance, coin_result=session['coin_result'],
                            balance_history=session['balance_history'], bet_amount=session.get('bet_amount', 0))
-
 
 @app.route('/game_over', methods=['GET'])
 @login_required
@@ -175,7 +153,6 @@ def game_over():
     db.session.commit()
 
     return render_template('game_over.html', balance=balance)
-
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -201,14 +178,12 @@ def register():
 
     return render_template('register.html')
 
-
 @app.route('/account_history', methods=['GET'])
 @login_required
 def account_history():
     coin_toss_results, balance_graph_data = get_coin_toss_results_and_balance_graph(current_user.id)
     return render_template('account_history.html', coin_toss_results=coin_toss_results,
                            balance_graph_data=json.dumps(balance_graph_data), enumerate=enumerate)
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -229,7 +204,6 @@ def login():
 
     return render_template('login.html')
 
-
 @app.route('/logout')
 @login_required
 def logout():
@@ -237,7 +211,5 @@ def logout():
     flash("You have been logged out.")
     return redirect(url_for('login'))
 
-
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=80)
     serve(app, host='0.0.0.0', port=80)
